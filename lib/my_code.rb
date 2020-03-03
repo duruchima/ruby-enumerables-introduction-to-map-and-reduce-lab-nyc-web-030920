a69f2f640394176266ddfe6b65cde0dcfd78360b
Duncan Uruchima <duruchima1@gmail.com>
2020-03-03 17:48:30 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -65,3 +65,12 @@ def reduce_to_all_true(source_array)
   end
   return true
 end
+
+def reduce_to_any_true(source_array)
+  i=0
+  while i<source_array.length do
+    return true if source_array[i] == true
+    i+=1
+  end
+  return true
+end
