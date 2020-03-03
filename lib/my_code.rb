ae71604e1e6fcdf6579eb49aeb9e98b8cb4d9b8c
Duncan Uruchima <duruchima1@gmail.com>
2020-03-03 17:24:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,11 @@
 # My Code here....
+def map_to_negativize(source_array)
+  new_array=[]
+  i=0
+  while i < source_array.length do
+    element = source_array[i]
+    result = element * -1
+    new_array << result
+  end  
+  new_array
+end
