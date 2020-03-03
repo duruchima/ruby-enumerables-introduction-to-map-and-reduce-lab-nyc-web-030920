fbdc579efd13617faae3a7bd676d5d3e62325c37
Duncan Uruchima <duruchima1@gmail.com>
2020-03-03 17:39:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -46,7 +46,7 @@ def map_to_square(source_array)
   new_array
 end
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point=index)
   new_array=[]
   i=0
   while i<source_array.length do
